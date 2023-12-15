c4eded83c78bd6fb3d55e5805aef72eef648d767
ludmil7703 <106627152+ludmil7703@users.noreply.github.com>
2023-12-15 22:18:46 +0200

    Fix URL

--- tests/ui/boardgames.test.js
+++ tests/ui/boardgames.test.js
@@ -1,7 +1,7 @@
 const { test, expect } = require('@playwright/test');
 
 test('Check boardgames page', async ({ page }) => {
-    await page.goto('http://localhost:8080/boardgames');
+    await page.goto('https://ludmil7703-exam-prep.onrender.com/boardgames');
     const list = await page.$('ul');
     expect(list).toBeTruthy();
   });
